a7854974d6ba1feb20846e21e36cfad95d2e12e7
Shaun Jacobsen <shaun.d.jacobsen@gmail.com>
2018-04-26 13:50:16 -0500

    Set up authenticated routes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import { Router, Route, Switch, Link, NavLink } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
-import Header from './../components/Header';
 import LoginPage from './../components/LoginPage';
 import ExpenseDashboardPage from './../components/ExpenseDashboardPage';
 import AddExpensePage from './../components/AddExpensePage';
 import EditExpensePage from './../components/EditExpensePage';
 import HelpPage from './../components/HelpPage';
 import NotFoundPage from './../components/NotFoundPage';
+import PrivateRoute from './PrivateRoute';
 
 export const history = createHistory();
 
 const AppRouter = () => (
   <Router history={history}>
     <div>
-      <Header />
       <Switch>
         <Route exact path="/" component={LoginPage} />
-        <Route exact path="/dashboard" component={ExpenseDashboardPage} />
-        <Route exact path="/create" component={AddExpensePage} />
-        <Route exact path="/edit/:id" component={EditExpensePage} />
+        <PrivateRoute exact path="/dashboard" component={ExpenseDashboardPage} />
+        <PrivateRoute exact path="/create" component={AddExpensePage} />
+        <PrivateRoute exact path="/edit/:id" component={EditExpensePage} />
         <Route exact path="/help" component={HelpPage} />
         <Route component={NotFoundPage} />
       </Switch>
