e270a09b93276e55af2c69dd1945b5e864cf1f83
Shaun Jacobsen <shaun.d.jacobsen@gmail.com>
2018-04-26 14:51:02 -0500

    Fixed critical error with PublicRoute

--- src/routers/PublicRoute.js
+++ src/routers/PublicRoute.js
@@ -6,7 +6,7 @@ export const PublicRoute = ({ isAuthenticated, component: Component, ...rest }) 
   <Route
     {...rest}
     component={props =>
-      isAuthenticated ? <Redirect to="/dashboard" /> : <Redirect to="/" />
+      isAuthenticated ? <Redirect to="/dashboard" /> : <Component {...props} />
     }
   />
 );
