e68bddfcbacccdd7c56332db90c210042b88601b
Shaun Jacobsen <shaun.d.jacobsen@gmail.com>
2018-04-26 10:40:06 -0500

    Firebase authentication

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { startLogout } from './../actions/auth';
 
-const Header = () => (
+export const Header = (props) => (
   <div>
     <h1>Expensify</h1>
-    <NavLink exact to='/' activeClassName='is-active'>Dashboard</NavLink>
-    <NavLink to='/create' activeClassName='is-active'>Create Expense</NavLink>
-    <NavLink to='/help' activeClassName='is-active'>Help</NavLink>
+    <NavLink exact to="/" activeClassName="is-active">
+      Dashboard
+    </NavLink>
+    <NavLink to="/create" activeClassName="is-active">
+      Create Expense
+    </NavLink>
+    <NavLink to="/help" activeClassName="is-active">
+      Help
+    </NavLink>
+    <button onClick={props.startLogout}>Log out</button>
   </div>
 );
 
-export default Header;
+const mapDispatchToProps = dispatch => {
+  return {
+    startLogout: () => dispatch(startLogout()),
+  };
+};
+
+export default connect(undefined, mapDispatchToProps)(Header);
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -12,26 +12,9 @@ const config = {
 firebase.initializeApp(config);
 
 const database = firebase.database();
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
-export { firebase, database as default };
-
-// database.ref('expenses').push({
-//   amount: 61250,
-//   description: 'Rent',
-//   createdAt: 1524682603,
-// });
-
-// database.ref('expenses').push({
-//   amount: 7000,
-//   description: 'Internet',
-//   createdAt: 1524682643,
-// });
-
-// database.ref('expenses').push({
-//   amount: 11000,
-//   description: 'Groceries',
-//   createdAt: 1524682650,
-// });
+export { firebase, googleAuthProvider, database as default };
 
 database.ref('expenses').on('value', snapshot => {
   const expenses = [];
@@ -42,60 +25,3 @@ database.ref('expenses').on('value', snapshot => {
     });
   });
 });
-
-// database
-//   .ref()
-//   .set({
-//     name: 'Shaun',
-//     age: 27,
-//     isSingle: false,
-//     location: {
-//       city: 'Chicago',
-//       country: 'USA',
-//     },
-//   })
-//   .then(() => {
-//     console.log('data saved');
-//   })
-//   .catch(e => {
-//     console.log('data save failed', e);
-//   });
-
-// // fetch data once
-// database
-//   .ref('location')
-//   .once('value')
-//   .then(snapshot => {
-//     const val = snapshot.val();
-//     console.log(val);
-//   })
-//   .catch(e => console.log('error', e));
-
-// // fetch data any time it changes (e.g. subscribed)
-// database.ref().on('value', snapshot => {
-//   console.log(snapshot.val());
-// });
-
-// // unsubscribe from updates
-// database.ref().off();
-
-// arrays
-// const notes = [
-//   {
-//     id: 12,
-//     title: 'First note',
-//     body: 'This is the note',
-//   },
-//   {
-//     id: 15,
-//     title: 'Second note',
-//     body: 'This is another note',
-//   },
-// ];
-
-// //database.ref('notes').set(notes);
-
-// database.ref('notes').push({
-//   title: 'Todo',
-//   body: 'Get groceries',
-// });
